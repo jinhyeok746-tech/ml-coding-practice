b1ef95d203665e318af9a35f2fe55970ddab1b3c
정진혁 <jinhyeok746@gmail.com>
2026-03-11 13:17:00 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -183,3 +183,37 @@ t2 * 10
 
 #딕셔너리 만들기
 dic = {'name':'Hong','phone':'01012345678','birth':'0814'}
+
+#원소 추가
+dic[1] = 'a'
+print(dic)
+
+dic['pet'] = 'dog'
+print(dic)
+
+#원소 삭제
+del dic[1]
+print(dic)
+
+#원소의 value 구하기
+print(dic['phone'])
+print(dic['name'])
+
+#key의 리스트 만들기
+print(dic.keys())
+print(list(dic.keys()))
+
+for key in dic.keys():
+    print(dic[key])
+
+#value의 리스트 만들기
+print(dic.values())
+print(list(dic.values()))
+
+#key, value 쌍 구하기
+print(dic.items())
+
+for key, value in dic.items():
+    print(key + ":" + value)
+    
+#원소 삭제
