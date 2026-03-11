0b243c3ef97d9fac3ce1ce4ee1c83b93ae74161b
정진혁 <jinhyeok746@gmail.com>
2026-03-11 13:35:50 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -302,7 +302,7 @@ while i <5:
     print('*' * i)
 
 #함수 정의
-def (sum1 (a,b))
+def sum1 (a,b):
     x = a + b
     return x
 
@@ -328,3 +328,4 @@ print(abs(-3.5))
 #그룹 자료형의 변수 x의 모든 원소가 참(0이 아닌 값)이면 True반환
 print(all([1,2,3,4]))
 print(all([4,-2,0.0,4]))
+
