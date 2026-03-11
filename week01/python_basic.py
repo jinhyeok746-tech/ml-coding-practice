448d3a37c79512b0c73b8a346ea12d559d9bc5db
정진혁 <jinhyeok746@gmail.com>
2026-03-11 13:31:41 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -298,30 +298,22 @@ for score in [90,25,67,45,93]:
 #while문
 i = 0
 while i <5:
+    i += 1
+    print('*' * i)
 
+#함수 정의
+def sum1 (a,b)
+    x = a + b
+    return x
 
+def sum2(*args):
+    x = 0
+    for i in args:
+        x += i
+    return x
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#함수 호출
+a = 5
+b = 3 
+print(sum1(a,b))
+print(sum1(3,5))
