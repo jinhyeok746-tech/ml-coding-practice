7725eca82cdf030b5a056cae6d57275c048481d6
정진혁 <jinhyeok746@gmail.com>
2026-03-11 13:03:26 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -80,6 +80,26 @@ a = "Python is difficult."
 print(a.replace("difficult", "funny"))
 print(a)
 
-#문자열 바꾸기
-a = "Python is difiicult."
+#문자열 나누기
+print(a.split())
+
+b = "a,b,c,d"
+print(b)
+print(b.split(','))
+
+#리스트 만들기
+a = [1,2,3]
+b = ['Life', 'is', 'too','short']
+c = [1,2,'Life','is']
+d = [1,2,[3,4],['Life','is']]
+
+#리스트 인덱싱
+print(d[0])
+print(d[3])
+print(d[3][-1])
+
+#리스트 슬라이싱
+print(d[0:3])
+
+#리스트 연결
 
