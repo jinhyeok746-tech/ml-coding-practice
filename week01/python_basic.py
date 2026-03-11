dd3abe9df7394d9f9d16af65e95cb3d59c2c6994
정진혁 <jinhyeok746@gmail.com>
2026-03-11 13:44:52 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -367,5 +367,24 @@ sum = lambda a, b: a + b
 print(sum(3,5))
 
 #최대, 최소값 반환
-print(max(1,4,2,8,6))
+print(max([1,4,2,8,6]))
 print(max("Python"))
+
+print(min([1,4,2,8,6]))
+print(min("Python"))
+
+# x의 y제곱 결과값 반환
+print(pow(2,4))
+
+# 사용자 입력으로 받은 값을 문자열로 반환 1
+c = input("값을 입력해주세요:")
+print(c)
+
+# 사용자 입력으로 받은 값을 문자열로 반환 2
+c = input("값을 입력해주세요:")
+print(c)
+
+#입력 받은 숫자에 해당되는 범위의 값을 반환
+print(range(5))
+print(list(range(5)))
+print(list(range(5,10)))
