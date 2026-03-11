e19fb6e1e270471907e8d933d8bf58fc82b6a595
정진혁 <jinhyeok746@gmail.com>
2026-03-11 13:00:56 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -72,4 +72,14 @@ print(d.rstrip())
 print(d.strip())
 
 #문자열 수정 (불가능)
+a = "pithon"
+#a[1] = 'y' #오류 발생
+
+#문자열 바꾸기
+a = "Python is difficult."
+print(a.replace("difficult", "funny"))
+print(a)
+
+#문자열 바꾸기
+a = "Python is difiicult."
 
