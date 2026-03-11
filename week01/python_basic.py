5db0a83aed3db65851b97ccc9429957f4d7bd01d
정진혁 <jinhyeok746@gmail.com>
2026-03-11 13:56:00 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -423,3 +423,10 @@ f = open('example.txt', 'w')
 for i in range(1, 6):
     data = "%d번째 줄입니다.\n" % i
     f.write(data)
+f.close()
+
+#파일에 내용 추가하기
+f = open('example.txt', 'w')
+for i in range(6, 11):
+    data = "%d번째 줄입니다.\n" % i
+    f.write(data)
