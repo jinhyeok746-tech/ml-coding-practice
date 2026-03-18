617818e2dedcd74b28d118943c03479e214cbfc3
정진혁 <jinhyeok746@gmail.com>
2026-03-18 13:11:44 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -207,3 +207,8 @@ print(np.arange(1,11))
 print(np.arange(1,11,2))
 print(np.linspace(1,100,10)) #1부터 100까지 균등한 간격의 값을 4개 추출
 
+A = np.array([4,16,25])
+print(np.sqrt(A))
+
+print(np.atange(8).reshape(2,4)+10)
+
