3d8507fa88696daf7f46d450b755e837a258ee82
정진혁 <jinhyeok746@gmail.com>
2026-03-18 12:36:43 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -64,3 +64,6 @@ print(more2015_tv.head())
 more2015_or_tv = netflix[(netflix['release_year'] > 2015) | (netflix['type'] == 'TV show')]
 print(more2015_or_tv.head())
 
+data = {
+    'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve','Frank','Grace','Hannah'],
+}
