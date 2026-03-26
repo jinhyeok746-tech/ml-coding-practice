bcfdba0be446b9d801a08d3cc7f3796e9c9f13d7
정진혁 <jinhyeok746@gmail.com>
2026-03-27 05:14:52 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -44,4 +44,7 @@ plt.bar(survived_counts.index, survived_counts,
         color = ['mediumorchid', 'darkviolet', 'indigo'])
 plt.title('Survived Counts by Embarked Port on Titanic')
 plt.xlabel('Embarked Port')
-plt.ylabel('Counts')
+plt.ylabel('Count')
+plt.xticks(survived_counts.index, ['Southampton', 'Cherbourg', 'Queenstown'])
+plt.legend(['Survived'], loc='upper right')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
