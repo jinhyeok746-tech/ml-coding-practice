4d622dcb8e0efb3d242e2c23d7a1e5d03d88ee85
정진혁 <jinhyeok746@gmail.com>
2026-03-27 05:13:52 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -42,4 +42,6 @@ print(survived_counts)
 #막대 그래프 그리기
 plt.bar(survived_counts.index, survived_counts,
         color = ['mediumorchid', 'darkviolet', 'indigo'])
-plt.title('Number of Survivors by Embarked Port')
+plt.title('Survived Counts by Embarked Port on Titanic')
+plt.xlabel('Embarked Port')
+plt.ylabel('Counts')
