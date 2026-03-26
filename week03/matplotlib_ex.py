19b4f52668b901ad16c7c5daf2cc4d3f6a03179a
정진혁 <jinhyeok746@gmail.com>
2026-03-27 05:08:22 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -1,2 +1,29 @@
 #-*- coding: utf-8 -*-
 #타이타닉 데이터셋 불러오기
+import pandas as pd
+
+#타이타닉 csv 파일 불러오기
+titanic = pd.read_csv('3.1.1.titanic.csv')
+
+#head( ) 함수를 출력하여 타이타닉 데이터셋의 구성을 간단히 살펴보기
+
+#데이터 처음 5개의 행 출력
+print(titanic.head())
+
+#열에 대한 요약 정보 확인
+print(titanic.info())
+
+"""### **선 그래프 : 객실 등급에 따른 생존율 표시하기**"""
+
+#객실 등급에 따른 생존자와 사망자의 평균 계산
+pclass_survived_mean = titanic.groupby('Pclass')['Survived'].mean().reset_index()
+pclass_survived_mean
+
+#맷플릇립 라이브러리 불러오기
+import matplotlib.pyplot as plt
+
+#선 그래프 그리기
+plt.plot(pclass_survived_mean['Pclass'], pclass_survived_mean['survived'],
+         marker='o', linestyle='-', color='violet')
+plt.title('Survival Rate Variation Across Passenger Classes')
+plt.xlabel('Survival Rate')
