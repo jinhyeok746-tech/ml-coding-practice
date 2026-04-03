1133b6949c52650b92365599f628d4b3377a311f
정진혁 <jinhyeok746@gmail.com>
2026-04-03 23:21:46 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -76,4 +76,9 @@ plt.savefig('./week04/Seaborn_Figure04.jpg')
 sns.jointplot(x='total_bill', y='tip', data=tips, kind='scatter')
 plt.savefig('./week04/Seaborn_Figure05.jpg')
 
-#
+# **관계 그래프**
+# pairplot( ) 그리기
+sns.pairplot(data=tips, hue='sex', diag_kind='hist', palette='husl')
+
+plt.suptitle('Pairplot with Histograms by Gender', y=1.05)
+plt.savefig('./week04/Seaborn_Figure06.jpg')
