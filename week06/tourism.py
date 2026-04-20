3c8859a744895e2ff6cb30af56efe2c8f3e557ac
정진혁 <jinhyeok746@gmail.com>
2026-04-20 13:38:29 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -51,4 +51,4 @@ def main():
                     print('[ %s_%s : %s]' % (natName, yyyymm, num))
                     print('-------------------------------------------')
                     jsonResult.append(('nat_name' : natName, 'nat_cd': nat_cd, 'yyyymm': yyyymm, 'visit_cnt': num))
-                    result
+                    result.append([natName, nat_cd, yyyymm, num])
