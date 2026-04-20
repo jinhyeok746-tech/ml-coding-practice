c9d3c07ed5eba289c1d1c5ae5746341ea5be074b
정진혁 <jinhyeok746@gmail.com>
2026-04-20 13:33:29 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -35,4 +35,9 @@ def main():
             for month in range(1, 13):
                 yyyymm = "{0}{1:0>2}".format(str(year), str(month))
                 jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd) #[CODE 2]
-                if (jsonData['response'])
+                if (jsonData['response']['header']['resiltMsg'] == 'OK'):
+                    #데이터가 없는 마지막 항목인 경우 ----------------------------------------
+                    if jsonData['response']['body']['items'] == '':
+                        dataEND = "{0}{1:0>2}".format(str(year), str(month-1))
+                        print("데이터 없음.... \n제공되는 통계 데이터는 %s년 %s월까지입니다." % (str(year), str(month-1)))
+                        
