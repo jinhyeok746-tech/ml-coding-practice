701a216209f9684f65f7ca01bcf027673c676fea
정진혁 <jinhyeok746@gmail.com>
2026-04-20 13:30:59 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -25,3 +25,14 @@ def main():
     result_df = pd.DataFrame(result, columns = columns)
     result_df.to_csv('./%s_%s_%d_%s.csv' % (natName, ed_cd, nStartYear, dataEND), index = False, encoding = 'cp949')
     
+    """### [CODE 3]"""
+    
+    def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
+        jsonResult = []
+        result = []
+        
+        for year in range(nStartYear, nEndYear+1):
+            for month in range(1, 13):
+                yyyymm = "{0}{1:0>2}".format(str(year), str(month))
+                jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd) #[CODE 2]
+                if (jsonData['response'])
