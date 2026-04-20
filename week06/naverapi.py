09149118ed30749b56dfea8a17677cfcb451753b
정진혁 <jinhyeok746@gmail.com>
2026-04-20 12:49:59 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -39,4 +39,15 @@ def main():
 def getNaverSearch(node, srcText, page_start, display):
     base = "https://openapi.naver.com/v1/search"
     node = "/%s.json" % node
-    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText),)
+    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
+    
+    url = base + node + parameters
+    responseDecode = getRequestUrl(url)
+    
+    if (responseDecode == None):
+        return None
+    else:
+        return json.loads(responseDecode)
+    
+    
+    
