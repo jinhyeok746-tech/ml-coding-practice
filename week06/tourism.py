2a7b7e3190827485a12427165b3995418a46f4fe
정진혁 <jinhyeok746@gmail.com>
2026-04-20 13:39:59 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -52,3 +52,7 @@ def main():
                     print('-------------------------------------------')
                     jsonResult.append({'nat_name' : natName, 'nat_cd': nat_cd, 'yyyymm': yyyymm, 'visit_cnt': num})
                     result.append([natName, nat_cd, yyyymm, num])
+                    
+    return (jsonResult, result, natName, ed)
+
+"""### [CODE 2]"""
