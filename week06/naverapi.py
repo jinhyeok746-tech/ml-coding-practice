2fa06c9d75df16a25619d4cc96774eab52881570
정진혁 <jinhyeok746@gmail.com>
2026-04-20 12:39:59 +0900

    Updated Changes

---
+++ week06/naverapi.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import urllib.request
+import datetime
+import json
+
+client_id = 'Client ID'
+client_secret = 'Client Secret'
+
+def main():
+    
+    node = 'news' #크롤링할 대상
+    srcText = input('검색어를 입력하세요: ')
+    
+    cnt = 0
+    jsonResult = []
+    
+    jsonResponse = getNaverSearch(node, srcText, 1, 100) #[CODE 2]
+    total = jsonRespnse['total']
