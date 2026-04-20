a4aa1f2c1befb2efc00b316e045f13bc5b3265f9
정진혁 <jinhyeok746@gmail.com>
2026-04-20 14:03:59 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -54,7 +54,7 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
                 result.append([natName, nat_cd, yyyymm, num])
                     
     return (jsonResult, result, natName, ed)
-    
+
 """### [CODE 2]"""
 
 def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
@@ -75,7 +75,7 @@ def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
     
 """### [CODE 1]"""
     
-def getRequesrUrl(url): #[CODE 1]
+def getRequestUrl(url): #[CODE 1]
     req = urllib.request.Request(url)
     try:
         response = urllib.request.urlopen(req)
