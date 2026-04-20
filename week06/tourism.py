0c506605b0173b81e50b77a8435e38f9ff5e43a9
정진혁 <jinhyeok746@gmail.com>
2026-04-20 13:43:59 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -64,4 +64,15 @@ def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
     parameters += "&NAT_CD=" + nat_cd
     parameters += "&ED_CD=" + ed_cd
     
+    url = service_url + parameters
     
+    responseDecode = getRequestUrl(url) #[CODE 1]
+    
+    if (responseDecode == None):
+        return None
+    else:
+        return json.loads(responseDecode)
+    
+    ""### [CODE 1]"""
+    
+    ef getRequesrUrl(url): 
