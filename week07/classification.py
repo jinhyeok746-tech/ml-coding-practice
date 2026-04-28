f52edfb99fdafb6c367e18ba81c282db109ec382
정진혁 <jinhyeok746@gmail.com>
2026-04-28 13:23:41 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from matplotlib.pylab import f
 from sklearn.datasets import fetch_openml
 
 mnist = fetch_openml('mnist_784', as_frame=False)
@@ -70,6 +71,8 @@ confusion_matrix(y_train_5, y_train_pred)
 print(cm)
 
 y_train_perfect_predictions = y_train_5 # 완벽한 분류기일 경우
+print(confusion_matrix(y_train_5, y_train_perfect_predictions)
 
-print(confusion_matrix
+# 정밀도와 재현율
+from sklearn.metrics import precision_score, recall_score
       (y_train_5, y_train_pred)) # == 3530 / (687 + 3530)
