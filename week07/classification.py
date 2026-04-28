91cc5ecb57aecdba4d11906f2dee482daac343ce
정진혁 <jinhyeok746@gmail.com>
2026-04-28 12:51:41 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 from sklearn.datasets import fetch_openml
-mnist = fetch_openml('mnist_')
+mnist = fetch_openml('mnist_784', as_frame=False)
+
+print(mnist.keys()) #data와 target만 사용
+
+x, y = mnist
