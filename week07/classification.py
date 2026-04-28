cde65017cfe2d1511ba83a05ff2008820f216af6
정진혁 <jinhyeok746@gmail.com>
2026-04-28 13:20:41 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -64,3 +64,7 @@ from sklearn.model_selection import cross_val_predict
 
 y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 
+from sklearn.metrics import confusion_matrix
+
+confusion_matrix(y_train_5, y_train_pred)
+print(cm)
