2ed4b9e6b4cf36341928d49858e172751510af3c
정진혁 <jinhyeok746@gmail.com>
2026-04-28 13:57:46 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -66,7 +66,7 @@ y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 
 from sklearn.metrics import confusion_matrix, precision_score
 
-confusion_matrix(y_train_5, y_train_pred)
+cm = confusion_matrix(y_train_5, y_train_pred)
 print(cm)
 
 y_train_perfect_predictions = y_train_5 # 완벽한 분류기일 경우
