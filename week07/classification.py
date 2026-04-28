3ea28afd9fe760b587597fc0719ce3d99cef978b
정진혁 <jinhyeok746@gmail.com>
2026-04-28 13:51:46 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -79,3 +79,29 @@ print(precision_score(y_train_5, y_train_pred)) # == 3530 / (687 + 3530)
 
 print(recall_score(y_train_5, y_train_pred)) # == 3530 / (1891 + 3530)
 
+from sklearn.metrics import f1_score
+
+print(f1_score(y_train_5, y_train_pred))
+
+# ROC 곡선
+from sklearn.metrics import roc_auc_score
+
+y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3,
+                             method="decision_function")
+roc_auc_score(y_train_5, y_scores)
+
+#다음은 실행하는데 몇 분이 걸릴 수 있음
+from sklearn.ensemble import RandomForestClassifier
+
+forest_clf = RandomForestClassifier(random_state=42)
+
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
+                                    mehod="predict_proba")
+
+y_scores_forest = y_probas_forest[:, 1]
+y_train_pred_forest = y_probas_forest[:, 1] >= 0.5 # 양성 확률 >= 50%
+
+print(f1_score(y_train_5, y_train_pred_forest))
+print(roc_auc_score(y_train_5, y_scores_forest))
+print(precision_score(y_train_5, y_train_pred_forest))
+print(recall_score(y_train_5, y_train_pred_forest))
