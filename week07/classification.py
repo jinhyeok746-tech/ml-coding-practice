10c1653b4cf9914d68e4743691128748c79e1ada
정진혁 <jinhyeok746@gmail.com>
2026-04-28 13:17:11 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -33,7 +33,7 @@ plt.subplots_adjust(wspace=0, hspace=0)
 plt.show()
 
 # train/test 데이터셋 나누기
-x_train, x_test, y_train, y_test = x[:60000], x[60000:], y[:60000], y[60000:]
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 
 # 이진 분류기 훈련
 y_train_5 = (y_train == '5') # 5는 True고, 다른 숫자는 모두 False
