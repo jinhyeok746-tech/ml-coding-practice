115df4a6b9907d8bd8732c6217889d3edba7b666
정진혁 <jinhyeok746@gmail.com>
2026-04-28 13:15:41 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-from email.mime import image
-
 from sklearn.datasets import fetch_openml
+
 mnist = fetch_openml('mnist_784', as_frame=False)
 
 print(mnist.keys()) #data와 target만 사용
 
-x, y = mnist.data, mnist.target
-print(x)
-print(x.shape) # 28x28개의 픽셀 특징을 가진 이미지 70,000개
+X, y = mnist.data, mnist.target
+print(X)
+print(X.shape) # 28x28개의 픽셀 특징을 가진 이미지 70,000개
 print(y)
 print(y.shape)
 
