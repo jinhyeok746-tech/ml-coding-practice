bcae0ea161a60a2505d0769c6c2e281f799ffe10
정진혁 <jinhyeok746@gmail.com>
2026-04-28 12:53:41 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -4,4 +4,18 @@ mnist = fetch_openml('mnist_784', as_frame=False)
 
 print(mnist.keys()) #data와 target만 사용
 
-x, y = mnist.data, mnist.from django.utils.translation import as _
+x, y = mnist.data, mnist.target
+print(x)
+print(x.shape) # 28x28개의 픽셀 특징을 가진 이미지 70,000개
+print(y)
+print(y.shape)
+
+import matplotlib.pyplot as plt
+
+def plot_digit(image_data):
+    image = image_data.reshape(28, 28)
+    plt.imshow(image, cmap="binary")
+    plt.axis("off")
+    
+some_digit = x[0]
+plot_digit(some_digit)
