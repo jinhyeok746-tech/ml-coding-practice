0cb08e8ff51d9815318214be94f1e111f5047fa7
정진혁 <jinhyeok746@gmail.com>
2026-04-28 13:02:41 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -40,3 +40,5 @@ x_train, x_test, y_train, y_test = x[:60000], x[60000:], y[:60000], y[60000:]
 y_train_5 = (y_train == '5') # 5는 True고, 다른 숫자는 모두 False
 y_test_5 = (y_test == '5')
 
+from sklearn.linear_model import SDGClassifier
+
