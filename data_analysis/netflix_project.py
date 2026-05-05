db1c16aada89049d6a84a881871befde45846d6f
정진혁 <jinhyeok746@gmail.com>
2026-05-05 13:07:13 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -171,7 +171,7 @@ country_order = ['United States', 'India', 'United Kingdom', 'Canada', 'Japan',
 netflix_age_country_unstack = netflix_age_country_unstack.loc[age_order, country_order]
 
 # 결측치 0으로 처리
-netflix_age_country_unstack = netflix_age_country_unstack.div(netflix_age_country_unstack.sum(axis=0), axis=1)
+netflix_age_country_unstack = netflix_age_country_unstack.div(netflix_age_country_unstack.fillna(0)
 netflix_age_country_unstack
 
 # 나이 그룹에 따른 국가별 넷플릭스 콘텐츠 비율 구하기
