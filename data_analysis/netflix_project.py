601a8e641ad849877d1d13f66f8b12a8847c3f47
정진혁 <jinhyeok746@gmail.com>
2026-05-05 12:55:12 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -132,3 +132,12 @@ genres
 
 plt.figure(figsize=(12, 6))
 
+sns.barplot(x=genres.values, y=genres.index, hue=genres.index, palette='RdGy')
+
+plt.title('Distribution of Genres for Movies and TV Shows on Netflix', fontsize=16)
+plt.xlabel('Count', fontsize=14)
+plt.ylabel('Genre', fontsize=14)
+plt.grid(axis='x')
+plt.show()
+
+netflix[netflix['title'].str.contains]
