f3121db73990866eed8a24eb53e5b04610334d2d
정진혁 <jinhyeok746@gmail.com>
2026-05-05 11:08:06 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -6,5 +6,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#데이터 불러오기
+#세션 저장소에 업로드한 csv 파일을 읽어 변수에 할당
+netflix = pd.read_csv('netflix_titles.csv')
+netflix.head()
 
+# .colums: 열 이름 확인
+netflix.columns
