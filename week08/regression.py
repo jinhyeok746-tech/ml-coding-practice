854590e6f91932a14e09d954680832e7154bc49d
정진혁 <jinhyeok746@gmail.com>
2026-05-12 14:12:05 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -69,3 +69,12 @@ lin_reg = LinearRegression()
 lin_reg.fit(X_poly, y)
 lin_reg.intercept_, lin_reg.coef_
 
+X_new = np.linspace(-3, 3, 100).reshape(100, 1)
+X_new_poly = poly_features.transform(X_new)
+y_new = lin_reg.predict(X_new_poly)
+
+plt.figure(figsize=(6, 4))
+plt.plot(X, y, "b.")
+plt.plot(X_new, y_new, "r-", linewidth=2, label="Predictions")
+plt.xlabel("$x_1$")
+plt.ylabel("$y$", rotation=0)
