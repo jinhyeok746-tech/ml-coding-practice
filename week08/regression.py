767381bf2f69c7e3065d7386f8266e2b9ea17d5b
정진혁 <jinhyeok746@gmail.com>
2026-05-12 14:35:32 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -147,4 +147,5 @@ print(iris.target_names)
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
-X = iris
+X = iris.data[["petal width (cm)"]].values
+y = iris.target_names
