007cc8b3a3a48111ee85e4227221f0ae1e6aea5f
정진혁 <jinhyeok746@gmail.com>
2026-05-12 14:00:35 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -2,3 +2,14 @@
 import matplotlib.pyplot as plt
 
 plt.rc('font', size=14)
+plt.rc('axes', labelsize=14, titlesize=14)
+plt.rc('legend', fontsize=14)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
+
+import numpy as np
+
+np.random.seed(42)
+m = 100
+x = 2 * np.random.rand(m, 1)
+y = 4 + 3 * X
