8af8963c8c8121a20f5fc33c520a0706c4ed6b3c
정진혁 <jinhyeok746@gmail.com>
2026-05-12 14:57:52 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -22,3 +22,11 @@ print(svm_clf.decision_fuction(X_new))
 
 # 비선형 SVM 분류
 
+from sklearn.datasets import make_moons
+from sklearn.preprocessing import PolynomialFeatures
+
+X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
+
+polynomial_svm_clf = make_pipeline(
+    Poly
+)
