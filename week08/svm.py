30ec03d46afde24406f8dc26c71aabd42469a041
정진혁 <jinhyeok746@gmail.com>
2026-05-12 14:59:22 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -30,5 +30,13 @@ X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
 polynomial_svm_clf = make_pipeline(
     PolynomialFeatures(degree=3),
     StandardScaler(),
-    LinearSVC(C=10, random_state=42)
+    LinearSVC(C=10, max_iter=10_000, dual=True, random_state=42)
 )
+polynomial_svm_clf.fit(X, y)
+
+# 다할식 패널
+
+from sklearn.svm import SVC
+
+poly_kernel_svm_clf = make_pipeline(StandardScaler(),
+                                    SVC(kernel="poly", degree=3, coef0=1, C=5, random_state=42))
