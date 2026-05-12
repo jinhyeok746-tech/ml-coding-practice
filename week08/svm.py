3d31b2389f5b8b2f8dd7f22f946f74a0451d3835
정진혁 <jinhyeok746@gmail.com>
2026-05-12 14:44:32 +0900

    Updated Changes

---
+++ week08/svm.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# 선형 SVM 분류
+
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+
+iris = load_iris(as_frame=True)
+X = iris.data[["petal length (cm)", "petal width (cm)"]].values
+y = (iris.target == 2) # Iris virginica
+
+svm_clf = make_pipeline(StandardScaler(),
+                        LinearSVC(C=1, dual=True, random_state=42))
+svm_clf.fit(X, y)
+
+X_new = [[5.5, 1.7], [5.0, 1.5]]
+
+print(svm_clf.predict(X_new))
+print(svm_clf.decision_fuction(X_new))
+
+# 비선형
