1775fb65990cc1ee7cc248015cffa444ce1e21cf
정진혁 <jinhyeok746@gmail.com>
2026-06-02 13:36:20 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -100,4 +100,15 @@ for k in (3, 4, 5, 6):
         ticks.append(pos + len(coeffs) / 2)
         pos += len(coeffs) + padding
         
-    plt.gca().xaxis.set_major_locator(FixedLocator(ticks))
+    plt.gca().yaxis.set_major_locator(FixedLocator(ticks))
+    plt.gca().yaxis.set_major_formatter(FixedFormatter(range(k)))
+    if k in (3, 5):
+        plt.ylabel("클러스터")
+        
+    if k in (5, 6):
+        plt.gca().set_xticks([-0.2, 0, 0.2, 0.4, 0.6, 0.8, 1])
+        plt.xlabel("실루엣 계수")
+    else:
+        plt.tick_params(labelbottom=False)
+        
+    plt.axcline(x=silhouette_scores[k - 2], xy1=(0, 0), xy2=(0, len(X)), color="red", linestyle="--")
