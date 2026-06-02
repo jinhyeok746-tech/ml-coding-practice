bb958f0d66f819164a7ae65462f6058e7776b545
정진혁 <jinhyeok746@gmail.com>
2026-06-02 13:51:45 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -129,4 +129,8 @@ print(dbscan.labels_[:10])
 print(dbscan.core_sample_indices_[:10])
 print(dbscan.components_)
 
-def plot_dbscan
+def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
+    core_mask = np.zeros_like(dbscan.labels_, dtype=bool)
+    core_mask[dbscan.core_sample_indices_] = True
+    anomalies_mask = dbscan.labels_ == -1
+    
