a6669745cb0b0f00e7a01d7b4a11fcd0a08b7317
정진혁 <jinhyeok746@gmail.com>
2026-06-02 13:55:45 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -155,3 +155,16 @@ def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
     plt.grid()
     plt.gca().set_axisbelow(True)
     
+dbscan2 = DBSCAN(eps=0.2)
+dbscan2.fit(X)
+
+plt.figure(figsize=(9, 3.2))
+
+plt.subplot(121)
+plot_dbscan(dbscan, X, size=100)
+
+plt.subplot(122)
+plot_dbscan(dbscan2, X, size=600, show_xlabels=False)
+
+plt.show()
+
