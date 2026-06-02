a4e1484e917083d1ec38996a8d4bfcfca15a885c
정진혁 <jinhyeok746@gmail.com>
2026-06-02 13:12:50 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -37,3 +37,16 @@ plt.grid()
 plt.show()
 
 # 각 샘플은 5개의 클러스터 중 하나에 할당
+print(y_pred)
+
+# 5개의 _센트로이드_(즉, 클러스터 중심)을 추정
+print(kmeans.cluster_centers_)
+
+print(kmeans.labels_)
+
+# 이너셔
+print(kmeans.inertia_)
+
+print(kmeans.score(X))
+
+kmeans_per_k = [KMeans(n_clusters=k, n_init=10, random_state=42).fit(X) for k in range(1, 10)]
