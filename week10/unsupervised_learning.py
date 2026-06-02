7ba11b5d15f20ec7a242377a0e723ada1103cd77
정진혁 <jinhyeok746@gmail.com>
2026-06-02 14:01:15 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -87,7 +87,7 @@ for k in (3, 4, 5, 6):
     y_pred = kmeans_per_k[k - 1].labels_
     silhouette_coefficients = silhouette_samples(X, y_pred)
     
-    padding = len(x) // 30
+    padding = len(X) // 30
     pos = padding
     ticks = []
     for i in range(k):
