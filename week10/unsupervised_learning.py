fa3b43bc82c712221ca18cc5de627cca8bbdea5d
정진혁 <jinhyeok746@gmail.com>
2026-06-02 13:22:50 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -83,12 +83,11 @@ plt.figure(figsize=(11, 9))
 
 for k in (3, 4, 5, 6):
     plt.subplot(2, 2, k - 2)
-    model = kmeans_per_k[k - 1]
-    y_kmeans = model.labels_
-    silhouette_samples_k = silhouette_samples(X, y_kmeans)
-    plt.scatter(X[:, 0], X[:, 1], c=silhouette_samples_k, cmap="Spectral", s=1)
-    plt.xlabel("$x_1$")
-    plt.ylabel("$x_2$", rotation=0)
-    plt.title(f"$k={k}$")
-    plt.gca().set_axisbelow(True)
-    plt.grid()
+    
+    y_pred = kmeans_per_k[k - 1].labels_
+    silhouette_coefficients = silhouette_samples(X, y_pred)
+    
+    padding = len(x) // 30
+    pos = padding
+    ticks = []
+    
